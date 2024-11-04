08d9dfaaa833ab1253a0f98f56e43a29987fd9e0
Birhanu27 <birhe903@gmail.com>
2024-11-04 22:38:44 +0300

    update file management
    add the main function to the code

--- file management1.cpp
+++ file management1.cpp
@@ -3,3 +3,37 @@
 #include <string>
 
 using namespace std;
+
+int main() {
+    int choice;
+    
+    do {
+        cout << "\nMenu:\n";
+        cout << "1. Display Files\n";
+        cout << "2. Search for a File\n";
+        cout << "3. Sort Files\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+        cin.ignore(); 
+
+        switch (choice) {
+            case 1:
+                displayFiles();
+                break;
+            case 2:
+                searchFile();
+                break;
+            case 3:
+                sortFiles();
+                break;
+            case 4:
+                cout << "Exiting the program.\n";
+                break;
+            default:
+                cout << "Invalid choice. Please try again.\n";
+        }
+    } while (choice != 4);
+
+    return 0;
+}
